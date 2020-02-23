76aadef055016dbab61e636a8187c0d6a27c4d1d
Grace <54523668+graceleung204@users.noreply.github.com>
2020-02-22 21:46:24 -0800

    updated

--- Tumblr/PhotosViewController.swift
+++ Tumblr/PhotosViewController.swift
@@ -47,12 +47,13 @@ class PhotosViewController: UIViewController, UITableViewDataSource, UITableView
         // Do any additional setup after loading the view.
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return 10
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
-        cell.textLabel?.text = "This is row \(indexPath.row)"
+        let cell = tableView.dequeueReusableCell(withIdentifier: "photosCell", for: indexPath) as! photosCell
+        
+        cell.rowname.text = "This is row \(indexPath.row)"
 
         return cell
     }
---
+++ Tumblr/photosCell.swift
@@ -0,0 +1,25 @@
+//
+//  photosCell.swift
+//  Tumblr
+//
+//  Created by Grace Leung on 2/21/20.
+//  Copyright © 2020 Grace Leung. All rights reserved.
+//
+
+import UIKit
+
+class photosCell: UITableViewCell {
+
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
